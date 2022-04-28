4d431870dc380b6093b4acbeeccc1b2398f3c7de
Reinan Kauã <contato.reinankaua@gmail.com>
2022-04-28 16:25:07 -0300

    test

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './sw.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -9,4 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 )
 
-serviceWorker.register();
+// serviceWorker.register();
