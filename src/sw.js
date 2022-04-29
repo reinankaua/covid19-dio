b9c9270402589b6675e8a8cd88c0e14a6fcfc84e
Reinan Kauã <contato.reinankaua@gmail.com>
2022-04-29 16:13:36 -0300

    test

---
+++ src/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'covid-19-cache';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/index.jsx',
+  '/App.jsx'
+]
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('v1').then(function (cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('active', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
